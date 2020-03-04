b75dc95201c0fd403f1cb7e9d84fe3ba58d3cdf0
WebDevJack <44706909+WebDevJack@users.noreply.github.com>
2020-03-04 11:00:21 +0000

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+/*
+   The functions.php file for the custom WordPress theme.
+*/
+
+if ( !function_exists("customtheme_setup") ) :
+
+  function theme_setup() {
+
+    add_theme_support("menus");
+
+    register_nav_menus ( array (
+      "primary"    =>  __( "Primary Menu", "customtheme" ),
+      "secondary"  =>  __( "Secondary Menu", "customtheme" )
+    ) );
+
+
+  }
+
+endif;
+
+add_action( 'after_setup_theme', 'customtheme_setup' );
